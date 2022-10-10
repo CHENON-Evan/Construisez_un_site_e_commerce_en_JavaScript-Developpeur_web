95535b1d02704b14a0db32cf467d423c8b21605d
evan <evan.chenon@gmail.com>
2022-10-10 17:54:30 +0200

    test : ajout div cart content

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,21 +1,25 @@
 const cart = []
 
-retrieveItemsFromCache()
-cart.forEach((item) => displayItem(item))
+retrieveProductsFromLocalStorage()
+cart.forEach((product) => displayProduct(product))
 
-function retrieveItemsFromCache() {
-    const numberOfItems = localStorage.length
-    for (let i = 0; i < numberOfItems; i++) {
-        const item = localStorage.getItem(localStorage.key(i))
-        const itemObject = JSON.parse(item)
-        cart.push(itemObject)
+function retrieveProductsFromLocalStorage() {
+    const numberOfProducts = localStorage.length
+    for (let i = 0; i < numberOfProducts; i++) {
+        console.log(i)
+        const product = localStorage.getItem(localStorage.key(i))
+        console.log("objet à la position", i, "est", product)
+        const productObject = JSON.parse(product)
+        cart.push(productObject)
     }
 }
 
-function displayItem(item) {
-    const article = makeArticle(item)
-    const div = makeImage(item)
-    article.appendChild(div)
+function displayProduct(product) {
+    const article = makeArticle(product)
+    const divImage = makeImage(product)
+    article.appendChild(divImage)
+    const cardProductContent = makeCartContent(product)
+    article.appendChild(cardProductContent)
     displayArticle(article)
 }
 
@@ -23,21 +27,26 @@ function displayArticle(article) {
     document.querySelector("#cart__items").appendChild(article)
 }
 
-function makeArticle(item) {
+function makeArticle(product) {
     const article = document.createElement("article")
-    article.classList.add("cart__item")
-    article.dataset.id = item.id
-    article.dataset.color = item.color
+    article.classList.add("card__item")
+    article.dataset.id = product.id
+    article.dataset.color = product.color
     return article
 }
 
-function makeImage(item) {
+function makeImage(product) {
     const div = document.createElement("div")
-    div.classList.add("card__item__img")
+    div.classList.add("cart__item__img")
 
     const image = document.createElement('img')
-    image.src = item.imageUrl
-    image.alt = item.altTxt
+    image.src = product.imageUrl
+    image.alt = product.altTxt
     div.appendChild(image)
     return div
+}
+
+function makeCartContent(product) {
+    const cardProductContent = document.createElement("div")
+    cardProductContent.classList.add("cart__item__content")
 }
